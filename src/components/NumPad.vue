dc1f7703cf7cc70203de37d9a4d32fe7cbd02aee
Chris Tudhope <chris@vrdistribution.com.au>
2019-03-15 01:32:00 +1030

    init commit - component file

---
+++ src/components/NumPad.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+/* import NumPad$ from './components/NumPad$.vue'; */
+export default {
+    name: "num-pad",
+    components: {},
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {},
+    watch: {}.
+    methods:   {},
+    filters: {},
+    mounted(){}
+}
+</script>
+
+<style scoped>
+    /* @import "./resources/assets/sass/variables"; */
+    /* local styles */
+</style>
+
+<style>
+    /* global styles */
+</style>
